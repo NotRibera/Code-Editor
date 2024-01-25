bda30d95cb68fd67c59c44428d7449e319b66399
Saurabh Patil <65603402+NotRibera@users.noreply.github.com>
2024-01-26 00:03:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function run() {
             let htmlcode = document.getElementById("html-code").value;
             let csscode = document.getElementById("css-code").value;
             let jscode = document.getElementById("js-code").value;
-            let output = document.getElementById("output");
+            let output = document.getElementById("result");
 
             output.contentDocument.body.innerHTML = htmlcode + "<style>" + csscode + "</style>";
             output.contentWindow.eval(jscode);
